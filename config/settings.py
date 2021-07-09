ba2fa9a990427902162826708f9df7a4f7773f2f
Oh Dany <ohdany@Oh-Dany.local>
2021-07-09 00:14:21 -0500

    Generando la primera vews basada en funcion y urls

--- config/settings.py
+++ config/settings.py
@@ -14,10 +14,9 @@ import os
 
 from pathlib import Path
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 import config.db as db
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
---
+++ core/erp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import myfirstview
+
+urlpatterns = [
+    path('uno/', myfirstview),
+    path('dos/', myfirstview),
+]
