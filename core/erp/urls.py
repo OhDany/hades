3ac4f23df987edf7dee05d2d6aaafc728b229e74
Oh Dany <ohdany@Oh-Dany.local>
2021-07-10 22:33:59 -0500

    Crear formulario con CreateView

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views.category.views import CategoryListView
+from .views.category.views import CategoryListView, CategoryCreateView
 
 app_name = 'erp'
 
 urlpatterns = [
     path('category/list', CategoryListView.as_view(), name='category_list'),
+    path('category/add', CategoryCreateView.as_view(), name='category_create'),
 ]
