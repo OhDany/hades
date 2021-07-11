f9250c66894672352856cce8dbb6d74a96f9597b
Oh Dany <ohdany@Oh-Dany.local>
2021-07-10 21:55:47 -0500

    Reescribiiendo el metodo post y usando ajax

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views.category.views import CategoryListView
 
+app_name = 'erp'
+
 urlpatterns = [
     path('category/list', CategoryListView.as_view(), name='category_list'),
 ]
--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,4 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 from core.erp.models import Category
 
@@ -14,6 +17,19 @@ def category_list(request):
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            data = Category.objects.get(pk=request.POST['id']).toJSON()
+        except Exception as e:
+            data['error'] = str(e)
+
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
