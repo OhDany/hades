a5ce78480a3b2e38be9b9971a88c045745d04305
Oh Dany <ohdany@Oh-Dany.local>
2021-07-12 12:11:30 -0500

    Editando un registro con ajax

--- core/erp/urls.py
+++ core/erp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views.category.views import CategoryListView, CategoryCreateView
+from .views.category.views import CategoryListView, CategoryCreateView, CategoryUpdateView
 
 app_name = 'erp'
 
 urlpatterns = [
     path('category/list/', CategoryListView.as_view(), name='category_list'),
     path('category/add/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/edit/<int:pk>/',
+         CategoryUpdateView.as_view(), name='category_update'),
 ]
