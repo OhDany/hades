54aa3e1754413aee73781c01431e8ce283c18f4b
Oh Dany <ohdany@Oh-Dany.local>
2021-07-12 15:41:44 -0500

    Ajustando el proyecto a template base

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
